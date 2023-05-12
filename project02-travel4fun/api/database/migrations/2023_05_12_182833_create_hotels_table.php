202efe00a029768e78863cb04b0d011c5e67def0
Guilherme da Costa Silva <fernandakagami@gmail.com>
2023-05-12 15:53:07 -0300

    Create 2023_05_12_182833_create_hotels_table.php

---
+++ project02-travel4fun/api/database/migrations/2023_05_12_182833_create_hotels_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('hotels', function (Blueprint $table) {
+            $table->id();
+            $table->longtext('description');
+            $table->string('postal_code');
+            $table->string('country_name');
+            $table->string('region_name');
+            $table->string('city');
+            $table->longtext('address');
+            $table->string('name');
+            $table->text('short_description');
+            $table->json('photos');
+            $table->decimal('stars', 3, 2);
+            $table->json('amenities');  
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('hotels');
+    }
+};
